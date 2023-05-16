75554c61049fe4d4f059dd3346127f0c639dcf6a
yung-slime <snakescitybigstateindia@gmail.com>
2023-05-16 23:23:56 +0530

    lvl 1 reached.

--- pyMtx.py
+++ pyMtx.py
@@ -1,25 +1,28 @@
-from numpy import ndarray, fromstring, reshape
+from numpy import ndarray, fromstring, reshape, linalg, dot, add
 
 class Matrix:
 
-    def __init__(self, rows: int, columns: int, elementsStream: str) -> None:
+    def __init__(self, rows: int, columns: int, elementsStream: str|ndarray) -> None:
 
         if not isinstance(rows, int):
-            raise TypeError("rows got to be of int type.")
+            raise TypeError("rows got to be of type int.")
         self.rows: int = rows
         
         if not isinstance(columns, int):
-            raise TypeError("columns got to be of int type.")
+            raise TypeError("columns got to be of type int.")
         self.columns: int = columns
 
-        if not isinstance(elementsStream, str):
-            raise TypeError("elementsStream got to be of str type.")
+        if not isinstance(elementsStream, str|ndarray):
+            raise TypeError("elementsStream got to be of type str|ndarray.")
         self.elementsStream: str = elementsStream
 
-        if len(self.elementsStream.split(" ")) != (rows * columns):
-            raise ValueError("total elements entered != {totalEl}.".format(totalEl = rows * columns))
-        self.matrix: ndarray = fromstring(self.elementsStream, dtype=int, sep=" ").reshape(self.rows, self.columns)
-    
+        if isinstance(elementsStream, str):
+            if len(self.elementsStream.split(" ")) != (rows * columns):
+                raise ValueError("total elements entered != {totalEl}.".format(totalEl = rows * columns))
+            self.matrix: ndarray = fromstring(self.elementsStream, dtype=int, sep=" ").reshape(self.rows, self.columns)
+        elif isinstance(elementsStream, ndarray):
+            self.matrix: ndarray = elementsStream
+
     def __str__(self) -> str:
         return str(self.matrix)
 
@@ -28,9 +31,12 @@ class Matrix:
         return repr(self.matrix)
     
     def __mul__(self, other: "Matrix") -> "Matrix":
-        pass
+        if self.columns == other.rows:
+            return Matrix(self.rows, other.columns, dot(self.matrix, other.matrix))
     
     def __add__(self, other: "Matrix") -> "Matrix":
-        pass
+        if ( self.rows == other.rows ) and ( self.columns == other.columns ):
+            return Matrix(self.rows, self.columns, add(self.matrix, other.matrix))
+        
 
-matA = Matrix(3, 2, "1 2 3 4 5 6")
+
